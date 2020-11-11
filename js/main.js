9099db9a9a620777e3558a6c2b836c073df0dcd7
Javier Andres Pedrozo Payares <7519384+pedrozopayares@users.noreply.github.com>
2020-11-11 10:57:29 -0500

    Logic for the Bingo is close complete.

--- js/main.js
+++ js/main.js
@@ -2,26 +2,55 @@
     function(){
 
         let systemMessages = document.getElementById('system-messages');
+        let bingoButton = document.getElementById('bingo-button');
         let inputFile = document.getElementById('data-file');
         let selectedFile;
-        let words;
+        let wordsToBingo;
 
-        inputFile.addEventListener("change", handleFiles, false);
+        inputFile.addEventListener("change", readFiles, false);
         systemMessages.innerHTML = "Archivo cargado.";
 
-        function handleFiles() {
+        function readFiles() {
             selectedFile = this.files[0];
             const reader = new FileReader();
-            reader.onload = function (e) {readSelectedFile(e);};
+            reader.onload = function (e) {extractWords(e);};
             reader.readAsText(selectedFile);
         }
 
-        function readSelectedFile(file) {
-            words = file.target.result.split(/\r?\n/);
-            console.log(words);
+        function extractWords(file) {
+            wordsToBingo = file.target.result.split(/\r?\n/);
+            console.log(wordsToBingo);
         }
-        
 
+        bingoButton.addEventListener("click", bingo, false);
+
+        function bingo() {
+            if (wordsToBingo.length > 0) {
+                selectedWord = selectRandomWord();
+                showRandomWord(selectedWord.word);
+                excludeFromAvalibleWords(selectedWord.index);
+                console.log(wordsToBingo);
+            } else {
+                systemMessages.innerHTML = "No hay más palabras para sortear el Bingo";
+            }
+        }
+
+        function selectRandomWord() {
+            let index = getRandomArbitrary(0, wordsToBingo.length);
+            return {'word': wordsToBingo[index], 'index': index};
+        }
+
+        function showRandomWord(wordToShow) {
+            systemMessages.innerHTML = "Palabra seleccionada: " + wordToShow;
+        }
+
+        function excludeFromAvalibleWords(index) {
+            wordsToBingo.splice(index, 1);
+        }
+
+        function getRandomArbitrary(min, max) {
+            return Math.floor(Math.random() * (max - min) + min);
+        }
 
 
     }
